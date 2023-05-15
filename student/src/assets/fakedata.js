f0ceae24f2ce5f4d64ec03dcfbe040d20023b19b
HexDan <123597766+Dan23H@users.noreply.github.com>
2023-05-15 17:49:26 -0500

    Delete student directory

--- student/src/assets/fakedata.js
+++
@@ -1,7 +0,0 @@
-export const fakedata = [{
-    "profile": "daniel.hernandez_val@uao.edu.co",
-    "password": "12345678"
-},{
-    "profile": "alfonso_paredes@uao.edu.co",
-    "password": "12ad52A_s"
-}]
--- student/src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export { Loginform } from "./loginform"
-export { TypeForm } from "./typeform"
-export { HomeContent } from "./homecontent"
-export { Navbar } from "./navbar"
